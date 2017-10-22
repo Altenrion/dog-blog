77546ea476a7e971307120933ebfb8576eaefa0f
Altenrion <landerfeld@gmail.com>
2017-10-22 16:02:24 +0300

    junk commit

--- subscribe.php
+++ subscribe.php
@@ -12,13 +12,12 @@ require 'vendor/autoload.php';
 if (isset($_POST['email'])) {
 
     $link = mysqli_connect("localhost", "altenrion_gbland", "Altenrion", "altenrion_gbland") or die("Error " . mysqli_error($link));
-//    $link = mysqli_connect("localhost","root","","altenrion_gbland") or die("Error " . mysqli_error($link));
 
     $query = "INSERT INTO subscription (`email`) VALUES ('{$_POST['email']}')" or die("Error in the consult.." . mysqli_error($link));
 
     $result = $link->query($query);
-    if (!empty($link->insert_id))
-        echo 'success';
+//    if (!empty($link->insert_id))
+//        echo 'success';
 
 
     $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
